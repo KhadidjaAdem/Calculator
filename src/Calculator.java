487c8b0494041117f652bc6f6c487f7307b0aaea
khadidja <khadidja.adam2014@gmail.com>
2019-01-19 17:03:00 -0500

    add divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,11 +4,14 @@ public class Calculator {
     }
 
     public static double subtract(double num1, double num2) {
-        return num1 - num2;
-        return result;
+        return  num1 - num2;
+
     }
     public static double multiply(double num1, double num2) {
         return num1 * num2;
     //this method was added
 }
+    public static double divide(double n1, double n2) {
+        return n1/n2;
+    }
 }
